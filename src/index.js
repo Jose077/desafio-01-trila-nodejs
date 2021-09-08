3a48fa3cdf174727f75cef53df4d760da47c5656
jose <josesandeson54@gmail.com>
2021-09-08 13:10:31 -0300

    Criada rota para cadastro de usuario

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username} = request.body;
+
+  const user = {
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  }
+
+  users.push(user);
+
+  response.status(201).send(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.header;
+
+  const user = user.find((e => username == e.username));
+
+  response.send(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
